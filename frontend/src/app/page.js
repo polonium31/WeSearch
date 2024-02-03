748bbb5d218d7eed88719c91544779076b4f8ca6
FarhaHaide <haidef11@mcmaster.ca>
2024-02-03 15:37:09 -0500

    test participant home

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import ParticipantLogin from "./components/participants/ParticipantLogin";
+import ParticipantHome from "./components/participants/ParticipantHome";
 
 export default function Home() {
   return (
    <main>
-      <ParticipantLogin></ParticipantLogin>
+      {/* <ParticipantLogin></ParticipantLogin> */}
+      <ParticipantHome/>
    </main>
   );
 }
